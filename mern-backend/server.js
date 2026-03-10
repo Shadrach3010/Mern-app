2016329a33a3817f756e85d15190bc04c79b552b
unknown <egertonhingston53@gmail.com>
2026-03-10 03:56:59 +0000

    Updates

--- mern-backend/server.js
+++ mern-backend/server.js
@@ -12,7 +12,10 @@ connectDB()
 const app = express()
 
 //Middleware
-app.use(cors())
+app.use(cors({
+    origin: "https://mern-app-six-dusky.vercel.app",
+    credentials: true
+}))
 app.use(express.json())
 
 //Test route
